c6f07800c1f335bc7771d616b1c3cb1d275a7006
Matt A <Zoxpler@gmail.com>
2022-12-08 22:48:07 -0700

    Added content

--- home/index.js
+++ home/index.js
@@ -1,9 +1,38 @@
 /*Made by Matthew Amurao*/
 
+//Change all id to tagname patch
+document.body.querySelectorAll('*').forEach(function(node) {
+    node.id = node.tagName.toLowerCase();
+});
+
+subheaders = ["omgitsasubheader","verycoolsubheader","subheadergoeshere","justanotherpersonalsite"];
+page = 0;
 lockHeading = true;
 
 if(lockHeading)
 {
-    document.body.insertBefore(document.getElementsByTagName("heading")[0], document.getElementsByTagName("main")[0]);
-    document.body.insertBefore(document.getElementsByTagName("navbar")[0], document.getElementsByTagName("main")[0]);
+    document.body.insertBefore(document.getElementById("heading"), document.getElementById("main"));
+    document.body.insertBefore(document.getElementById("navbar"), document.getElementById("main"));
 }
+
+document.getElementById("heading2").innerHTML = subheaders[Math.floor(Math.random() * subheaders.length)]
+Array.from(document.getElementById("pages").children).forEach(element => {
+    page = document.createElement("a");
+    page.onclick = function(){pageSelect(Array.from(document.getElementById("navbar").children).indexOf(this))};
+    page.innerHTML = element.getAttribute("name");
+    document.getElementById("navbar").appendChild(page);
+});
+
+function pageSelect(page)
+{
+    Array.from(document.getElementById("navbar").children).forEach(element => {
+        element.style.backgroundColor = "initial";
+    });
+    Array.from(document.getElementById("pages").children).forEach(element => {
+        element.style.display = "none";
+    });
+    document.getElementById("pages").children.item(page).style.display = "initial";
+    document.getElementById("navbar").children.item(page).style.backgroundColor = "var(--minjudarkT)";
+}
+
+pageSelect(page);
