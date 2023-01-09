a6b888f74f2438eb3f735141032c47189fb924ce
Matt A <Zoxpler@gmail.com>
2023-01-09 03:36:08 -0700

    Fixed page contents
    First iteration of page contents, removed place holders
    
    Added image support

--- home/index.js
+++ home/index.js
@@ -1,7 +1,8 @@
 /*Made by Matthew Amurao*/
 
 //Change all id to tagname patch
-document.body.querySelectorAll('*').forEach(function(node) {
+document.body.querySelectorAll("*").forEach(function(node)
+{
     node.id = node.tagName.toLowerCase();
 });
 
@@ -15,12 +16,24 @@ if(lockHeading)
     document.body.insertBefore(document.getElementById("navbar"), document.getElementById("main"));
 }
 
+//Handle images
+function loadPageImages(page)
+{
+    Array.from(page.getElementsByClassName("image")).forEach(element => {
+        element.style = "font-size: initial; text-align: right; padding-left: 40px; display: inline-block; background: url(\"../images/"+element.id+"\") no-repeat;";
+    });
+}
+
+//Handle pages
 document.getElementById("heading2").innerHTML = subheaders[Math.floor(Math.random() * subheaders.length)]
 Array.from(document.getElementById("pages").children).forEach(element => {
-    element.innerHTML = element.innerHTML.replace("\\n", "<br />");
+    fetch(element.id).then(response => response.text()).then(textString => {
+        element.innerHTML = textString;
+        loadPageImages(element);
+    }).catch(function() {element.innerHTML = "Unable to fetch data."});
     page = document.createElement("a");
     page.onclick = function(){pageSelect(Array.from(document.getElementById("navbar").children).indexOf(this))};
-    page.innerHTML = "<span style = 'padding: initial' class = 'material-symbols-rounded'>" + element.getAttribute("icon") + "</span> " + element.getAttribute("name");
+    page.innerHTML = "<span style = \"padding: initial\" class = \"material-symbols-rounded\">" + element.getAttribute("icon") + "</span> " + element.getAttribute("name");
     document.getElementById("navbar").appendChild(page);
 });
 
