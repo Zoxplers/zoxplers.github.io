d9e3d17274e56adec71f00c58620dc7d309d230c
Matt A <Zoxpler@gmail.com>
2023-07-27 19:04:38 -0600

    update /test

--- test/index.js
+++ test/index.js
@@ -2,6 +2,6 @@
 Array.from(document.getElementsByTagName("pages")[0].children).forEach(page =>{
     //Navmenu items
     let button = document.createElement("navitem");
-    button.innerHTML = "<span class = \"material-symbols-rounded\">" + page.getAttribute("icon") + "</span>" + page.getAttribute("name");
+    button.innerHTML = "<span class = \"material-symbols-rounded\">" + page.getAttribute("icon") + "</span><text>" + page.getAttribute("name") + "</text>";
     document.getElementsByTagName("navmenu")[0].appendChild(button);
 });
