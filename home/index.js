59691ced69e88b98c62662f85438b87448f76d47
Matt A <Zoxpler@gmail.com>
2023-10-12 04:13:28 -0600

    Fixed the heading

--- home/index.js
+++ home/index.js
@@ -1,7 +1,7 @@
 /*Made by Zoxplers*/
+
+//Resize
 resize = false;
-toggles = [];
-logoAmount = 6;
 
 function resizeBody()
 {
@@ -16,12 +16,8 @@ function resizeBody()
         document.getElementsByTagName("mainbg")[0].className = "landscape";
         document.documentElement.style.setProperty("--headingSize", "10vh");
         document.documentElement.style.setProperty("font-size", "max(1vw, 1.5vh)");
-        document.getElementsByTagName("name")[0].onresize = function()
-        {
-            console.log("A");
-        }
     }
-};
+}
 
 resizeBody();
 
@@ -30,23 +26,31 @@ window.addEventListener("resize", function()
     clearTimeout(resize);
     resize = setTimeout(resizeBody, 250);
 });
+//Resize End
 
-document.getElementsByTagName("text")[0].onclick = function()
+//NameClick
+nameToggle = true;
+nameElem = document.getElementsByTagName("name")[0];
+function nameClick()
 {
-    if(toggles.includes(document.getElementsByTagName("text")[0]))
+    nameToggle = !nameToggle;
+    if(nameToggle)
     {
-        document.getElementsByTagName("name")[0].style.removeProperty("mix-blend-mode");
-        document.getElementsByTagName("text")[0].style.removeProperty("text-shadow");
-        document.getElementsByTagName("text")[0].style.removeProperty("color");
-        toggles.splice(toggles.indexOf(document.getElementsByTagName("text")[0]));
+        document.getElementById("foreground").appendChild(nameElem).onclick = nameClick;
+        nameElem.style.removeProperty("text-shadow");
+        nameElem.style.removeProperty("color");
     }
     else
     {
-        document.getElementsByTagName("name")[0].style.setProperty("mix-blend-mode", "multiply");
-        document.getElementsByTagName("text")[0].style.setProperty("text-shadow", "0 0 3px white");
-        document.getElementsByTagName("text")[0].style.setProperty("color", "white");
-        toggles.push(document.getElementsByTagName("text")[0])
+        document.getElementById("midground").appendChild(nameElem).onclick = nameClick;
+        nameElem.style.setProperty("text-shadow", "0 0 3px white");
+        nameElem.style.setProperty("color", "white");
     }
-};
+}
+nameElem.onclick = nameClick;
 
-document.getElementsByTagName("logo")[0].children.item(1).setAttribute("src", "../assets/logo"+Math.ceil(Math.random() * logoAmount)+".png");
+//NameClick End
+
+//Logo
+logoAmount = 6;
+//Logo End
