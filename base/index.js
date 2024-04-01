1d7e13ff542b4ef3d69f4e434cf07b4a8082a5d6
Matt A <Zoxpler@gmail.com>
2024-04-01 22:12:38 +0800

    minor base updates
    Updated /base files

--- base/index.js
+++ base/index.js
@@ -4,6 +4,7 @@
 document.body.style.animationDelay = Math.floor(Math.random() * -parseInt(getComputedStyle(document.body).animationDuration)) + "s";
 document.body.style.background = "no-repeat linear-gradient(to bottom left, var(--nako), var(--chaeyeon), var(--chaewon), var(--yujin), var(--minju), var(--sakura), var(--hitomi), var(--yena), var(--yuri), var(--hyewon), var(--wonyoung), var(--eunbi), var(--eunbi),var(--wonyoung), var(--hyewon), var(--yuri), var(--yena), var(--hitomi), var(--sakura), var(--minju), var(--yujin), var(--chaewon), var(--chaeyeon), var(--nako), var(--nako), var(--chaeyeon), var(--chaewon), var(--yujin), var(--minju), var(--sakura), var(--hitomi), var(--yena), var(--yuri), var(--hyewon), var(--wonyoung), var(--eunbi))";
 document.body.style.backgroundSize = "316% 316%"; //Random numbers?
+//Backgroun End
 
 //Resize
 resize = false;
@@ -25,6 +26,7 @@ function resizeBody()
         });
     }
 }
+//Resize End
 
 resizeBody();
 
@@ -34,3 +36,10 @@ window.addEventListener("resize", function()
     resize = setTimeout(resizeBody, 250);
 });
 //Resize End
+
+//Title
+if(window.location.protocol == "http:" || window.location.protocol == "https:")
+{
+    document.title = "Zoxplers - " + window.location.pathname.split("/")[1].charAt(0).toUpperCase() + window.location.pathname.split("/")[1].slice(1);
+}
+//Title End
