c0672caa025a50bae62d4067acaccd9b9ba38307
Matt A <Zoxpler@gmail.com>
2025-01-17 07:15:58 -0700

    Portfolio update
    Uploaded content for /portfolio

--- base/index.js
+++ base/index.js
@@ -1,25 +1,25 @@
-/*Made by Zoxplers*/
+/* Made by Zoxplers */
 
-//Background
+// Background
 document.body.style.animationDelay = Math.floor(Math.random() * -parseInt(getComputedStyle(document.body).animationDuration)) + "s";
 document.body.style.background = "no-repeat linear-gradient(to bottom left, var(--nako), var(--chaeyeon), var(--chaewon), var(--yujin), var(--minju), var(--sakura), var(--hitomi), var(--yena), var(--yuri), var(--hyewon), var(--wonyoung), var(--eunbi), var(--eunbi),var(--wonyoung), var(--hyewon), var(--yuri), var(--yena), var(--hitomi), var(--sakura), var(--minju), var(--yujin), var(--chaewon), var(--chaeyeon), var(--nako), var(--nako), var(--chaeyeon), var(--chaewon), var(--yujin), var(--minju), var(--sakura), var(--hitomi), var(--yena), var(--yuri), var(--hyewon), var(--wonyoung), var(--eunbi))";
 document.body.style.backgroundSize = "316% 316%"; //Random numbers?
-//Background End
+// Background End
 
-//HomeButton
+// HomeButton
 document.body.innerHTML = "<homebutton><span class=\"material-symbols-outlined\">House</span><a href=\"/\">Home</a></homebutton>" + document.body.innerHTML;
-//HomeButton End
+// HomeButton End
 
-//Main
+// Main
 let background = document.getElementById("background");
 let foreground = document.getElementById("foreground");
 foreground.addEventListener("scroll", e => {
     background.scrollTop = foreground.scrollTop
     background.scrollLeft = foreground.scrollLeft
 });
-//Main End
+// Main End
 
-//Resize
+// Resize
 resize = false;
 function resizeBody()
 {
@@ -46,11 +46,85 @@ window.addEventListener("resize", function()
     clearTimeout(resize);
     resize = setTimeout(resizeBody, 250);
 });
-//Resize End
+// Resize End
 
-//Title
+// Title
 if(window.location.protocol == "http:" || window.location.protocol == "https:")
 {
     document.title = "Zoxplers - " + window.location.pathname.split("/")[1].charAt(0).toUpperCase() + window.location.pathname.split("/")[1].slice(1);
 }
-//Title End
+// Title End
+
+// Tooltips
+function tooltip(obj, backgroundBool, text, align)
+{
+    var anim;
+    let tooltipElem = document.createElement("tooltip");
+    tooltipElem.innerHTML = text;
+    (backgroundBool ? background : foreground).append(tooltipElem);
+
+    function enableTooltip()
+    {
+        tooltipElem.classList.remove("hidden");
+        objRect = obj.getBoundingClientRect();
+        if(align == "left")
+        {
+            tooltipElem.style.setProperty("left", (objRect.left - tooltipElem.offsetWidth) + "px");
+            tooltipElem.style.setProperty("top", objRect.top + "px");
+        }
+        else if(align == "right")
+        {
+            tooltipElem.style.setProperty("left", objRect.right + "px");
+            tooltipElem.style.setProperty("top", objRect.top + "px");
+        }
+        else if(align == "top")
+        {
+            tooltipElem.style.setProperty("left", (objRect.left + objRect.width / 2 - tooltipElem.offsetWidth / 2) + "px");
+            tooltipElem.style.setProperty("top", (objRect.top - tooltipElem.offsetHeight) + "px");
+        }
+        else if(align == "bottom")
+        {
+            tooltipElem.style.setProperty("left", (objRect.left + objRect.width / 2 - tooltipElem.offsetWidth / 2) + "px");
+            tooltipElem.style.setProperty("top", objRect.bottom + "px");
+        }
+        else
+        {
+            tooltipElem.style.setProperty("left", objRect.left + "px");
+            tooltipElem.style.setProperty("top", objRect.top + "px");
+        }
+        tooltipElem.style.setProperty("--visibility", "1");
+        clearInterval(anim);
+        anim = setInterval(animFunc, 5);
+    }
+
+    function disableTooltip()
+    {
+        tooltipElem.style.setProperty("--visibility", "0");
+        clearInterval(anim);
+        anim = setInterval(animFunc, 5);
+    }
+
+    function animFunc()
+    {
+        alpha = 0.2;
+        opacity = Number(tooltipElem.style.getPropertyValue("opacity"));
+        visibility = Number(tooltipElem.style.getPropertyValue("--visibility"));
+        opacity = opacity + alpha * (visibility - opacity);
+        tooltipElem.style.setProperty("opacity", opacity);
+        if(opacity > 0.9 || opacity < 0.1)
+        {
+            tooltipElem.style.setProperty("opacity", visibility);
+            if(opacity < 0.1)
+            {
+                tooltipElem.classList.add("hidden");
+            }
+            clearInterval(anim);
+        }
+    }
+
+    obj.onmouseover = enableTooltip;
+    obj.onfocus = enableTooltip;
+    obj.onmouseout = disableTooltip;
+    disableTooltip();
+}
+// Tooltips End
