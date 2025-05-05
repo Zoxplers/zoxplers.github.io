ffea163b306e63fe5bec5701dbc75e3050348881
Matt A <Zoxpler@gmail.com>
2025-05-05 13:21:56 -0600

    Redirect branch

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+const App = () => {
+  window.location.replace("https://zoxplers.com");
+  return (
+    <div>
+      <a href="https://zoxplers.com">Redirect</a>
+    </div>
+  );
+};
+
+export default App;
